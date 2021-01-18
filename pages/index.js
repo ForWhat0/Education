6b3fdf6cc4f15f3e87900094c8ceb5a746512857
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-18 20:40:07 +0200

    hkg

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,9 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+
+
+
 import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -275,11 +275,10 @@ export default function Home({contacts,locale,menu,news,events,data,services,all
             <Typography variant="h4" component="h1" gutterBottom>
               Next.js example
             </Typography>
-            <Button variant="contained" color="primary" component={Link} naked href="/">
+            <Button variant="contained" color="primary"  naked href="/">
               Go to the main page
             </Button>
-            <ProTip />
-            <Copyright />
+
           </Box>
         </Container>
         {popularProjectsData?.projects?.length > 0 &&<ProjectsWrapper locale={locale}  posts={popularProjectsData}/>}
