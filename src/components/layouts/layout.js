10cef79a288c007b0e6d9bd0a2cbc80d65abc79c
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-23 19:31:25 +0200

    asdas

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -1,60 +1,15 @@
 
-import { useDispatch, useSelector} from "react-redux"
-import Menu from "../burgerMenu/menu";
-import { useRef} from 'react';
-
-import {
-    useOnClickOutside
-} from '../hooks/hooks';
-import {actionClickBurger} from "../../redux/actions/actions";
-
-
-
-import {RouterLink} from "../routerLink/routerLink";
-import { Modal } from "../modal/modal";
+import { useSelector} from "react-redux"
 import {BubbleBg} from "../bubbleBg/bubbleBg";
-
-
-
-export  const Layout = ({showLinks,menu, header}) => {
+export  const Layout = () => {
     const {visuallyImpairedMode} = useSelector(state=>state.app)
-
-
-    const dispatch = useDispatch()
-    const {visuallyImpairedModeWhiteTheme} = useSelector(state=>state.app)
-
-    const {fontSize} = useSelector(state=>state.app)
-
-    const node = useRef();
-    const {menuBurgerIsOpen} = useSelector(state=>state.app)
-    useOnClickOutside(node,  () => menuBurgerIsOpen === true  &&  dispatch(actionClickBurger()));
 
 
 
     return (
-        <>
 
-
-            <Modal/>
-
-                <div  ref={node}>
-                    {header}
-                    <Menu menu={menu}/>
-                </div>
-            {
                 !visuallyImpairedMode && <BubbleBg/>
-            }
-            {
-                showLinks && <RouterLink/>
-            }
 
-
-
-
-
-
-            
-        </>
     );
 };
 
