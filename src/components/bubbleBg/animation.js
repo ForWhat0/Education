878f5fe18bd2eae31866f2a0087c7d70cbdb1efb
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-24 12:38:07 +0200

    hh

--- src/components/bubbleBg/animation.js
+++ src/components/bubbleBg/animation.js
@@ -5,10 +5,16 @@ export default function animationButton(){
     $(window).bind('scroll',function(){
         parallaxScroll()
     })
-
+    let scrolled = $(window).scrollTop();
     function parallaxScroll(){
-        let scrolled = $(window).scrollTop();
+
         $('#parallax-lvl-3').css('top',(0-(scrolled*.5))+'px');
     }
+
+    return (
+        <h1>
+            {scrolled}
+        </h1>
+    )
 
 }
