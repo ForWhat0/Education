c1b544096a2c301c281226b54fda32c368a06045
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-25 00:31:55 +0200

    hh

--- src/components/bubbleBg/bubbleBg.js
+++ src/components/bubbleBg/bubbleBg.js
@@ -3,24 +3,22 @@ import {useEffect} from "react";
 
 export const BubbleBg = ()=>{
     useEffect(()=>{
-         animationButton()
+        animationButton()
     },[])
     return(
         <div id="wrapper">
-
-
-            <div id="parallax-lvl-3">
-                <div id="b0-1" className="bubble size3 blue">&nbsp;</div>
-                <div id="b0-2" className="bubble size2 blue">&nbsp;</div>
-                <div id="b0-3" className="bubble size1 blue">&nbsp;</div>
-                <div id="b0-4" className="bubble size4 yellow">&nbsp;</div>
-                <div id="b0-5" className="bubble size3 blue">&nbsp;</div>
-                <div id="b0-6" className="bubble size2 blue">&nbsp;</div>
-                <div id="b0-7" className="bubble size2 yellow">&nbsp;</div>
-                <div id="b0-8" className="bubble size1 yellow">&nbsp;</div>
-          
+            <div id="innerWrapper">
+                <div id="parallax-lvl-3">
+                    <div id="b0-1" className="bubble size3 blue">&nbsp;</div>
+                    <div id="b0-2" className="bubble size2 blue">&nbsp;</div>
+                    <div id="b0-3" className="bubble size1 blue">&nbsp;</div>
+                    <div id="b0-4" className="bubble size4 yellow">&nbsp;</div>
+                    <div id="b0-5" className="bubble size3 blue">&nbsp;</div>
+                    <div id="b0-6" className="bubble size2 blue">&nbsp;</div>
+                    <div id="b0-7" className="bubble size2 yellow">&nbsp;</div>
+                    <div id="b0-8" className="bubble size1 yellow">&nbsp;</div>
+                </div>
             </div>
-
         </div>
     )
 }
