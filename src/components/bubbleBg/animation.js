dca7145767c5b5bcabe9857bca263404cbd57f93
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-25 02:14:12 +0200

    hh

--- src/components/bubbleBg/animation.js
+++ src/components/bubbleBg/animation.js
@@ -1,14 +1,11 @@
-import $ from 'jquery';
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
 
-export default function animationButton(){
-
-    $(window).bind('scroll',function(){
-        parallaxScroll()
-    })
-
-    function parallaxScroll(){
-        let scrolled = $(window).scrollTop();
-        $('#parallax-lvl-3').css('margin-top',(0-(scrolled*.5))+'px');
-    }
-
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-lvl-0').css('top',(0-(scrolled*.25))+'px');
+    $('#parallax-lvl-1').css('top',(0-(scrolled*.5))+'px');
+    $('#parallax-lvl-2').css('top',(0-(scrolled*.75))+'px');
+    $('#parallax-lvl-3').css('top',(0-(scrolled*.9))+'px');
 }
--- src/components/bubbleBg/bubbleBg.js
+++ src/components/bubbleBg/bubbleBg.js
@@ -7,18 +7,49 @@ export const BubbleBg = ()=>{
     },[])
     return(
         <div id="wrapper">
-            <div id="innerWrapper">
-                <div id="parallax-lvl-3">
-                    <div id="b0-1" className="bubble size3 blue">&nbsp;</div>
-                    <div id="b0-2" className="bubble size2 blue">&nbsp;</div>
-                    <div id="b0-3" className="bubble size1 blue">&nbsp;</div>
-                    <div id="b0-4" className="bubble size4 yellow">&nbsp;</div>
-                    <div id="b0-5" className="bubble size3 blue">&nbsp;</div>
-                    <div id="b0-6" className="bubble size2 blue">&nbsp;</div>
-                    <div id="b0-7" className="bubble size2 yellow">&nbsp;</div>
-                    <div id="b0-8" className="bubble size1 yellow">&nbsp;</div>
-                </div>
+            <div id="parallax-lvl-0">
+                <div id="b0-1" className="bubble size4 white">&nbsp;</div>
+                <div id="b0-2" className="bubble size3 white">&nbsp;</div>
+                <div id="b0-3" className="bubble size2 white">&nbsp;</div>
+                <div id="b0-4" className="bubble size2 white">&nbsp;</div>
+                <div id="b0-5" className="bubble size3 white">&nbsp;</div>
+                <div id="b0-6" className="bubble size2 white">&nbsp;</div>
+
+                <div id="b0-7" className="bubble size4 white">&nbsp;</div>
+                <div id="b0-8" className="bubble size3 white">&nbsp;</div>
+                <div id="b0-9" className="bubble size2 white">&nbsp;</div>
+                <div id="b0-10" className="bubble size2 white">&nbsp;</div>
+                <div id="b0-11" className="bubble size3 white">&nbsp;</div>
+                <div id="b0-12" className="bubble size2 white">&nbsp;</div>
             </div>
+
+            <div id="parallax-lvl-1">
+                <div id="b1-1" className="bubble size1 blue">&nbsp;</div>
+                <div id="b1-2" className="bubble size4 blue">&nbsp;</div>
+                <div id="b1-3" className="bubble size4 orange">&nbsp;</div>
+                <div id="b1-4" className="bubble size2 pink">&nbsp;</div>
+                <div id="b1-5" className="bubble size1 green">&nbsp;</div>
+                <div id="b1-6" className="bubble size2 purple">&nbsp;</div>
+            </div>
+
+            <div id="parallax-lvl-2">
+                <div id="b2-1" className="bubble size2 blue">&nbsp;</div>
+                <div id="b2-2" className="bubble size1 pink">&nbsp;</div>
+                <div id="b2-3" className="bubble size2 green">&nbsp;</div>
+                <div id="b2-4" className="bubble size2 purple">&nbsp;</div>
+                <div id="b2-5" className="bubble size3 pink">&nbsp;</div>
+                <div id="b2-6" className="bubble size1 orange">&nbsp;</div>
+            </div>
+
+            <div id="parallax-lvl-3">
+                <div id="b3-1" className="bubble size2 green">&nbsp;</div>
+                <div id="b3-2" className="bubble size2 pink">&nbsp;</div>
+                <div id="b3-3" className="bubble size1 green">&nbsp;</div>
+                <div id="b3-4" className="bubble size4 green">&nbsp;</div>
+                <div id="b3-5" className="bubble size1 purple">&nbsp;</div>
+                <div id="b3-6" className="bubble size2 blue">&nbsp;</div>
+            </div>
+
         </div>
     )
 }
