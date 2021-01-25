9dd79c4e2c0286baa7627ae8d186d2840b4d4e1c
Roman Zozulia <7923-7749-1@plus4u.net>
2021-01-25 02:21:10 +0200

    hh

--- src/components/bubbleBg/animation.js
+++ src/components/bubbleBg/animation.js
@@ -1,11 +1,17 @@
-$(window).bind('scroll',function(e){
-    parallaxScroll();
-});
+import $ from 'jquery';
 
-function parallaxScroll(){
-    var scrolled = $(window).scrollTop();
-    $('#parallax-lvl-0').css('top',(0-(scrolled*.25))+'px');
-    $('#parallax-lvl-1').css('top',(0-(scrolled*.5))+'px');
-    $('#parallax-lvl-2').css('top',(0-(scrolled*.75))+'px');
-    $('#parallax-lvl-3').css('top',(0-(scrolled*.9))+'px');
+export default function animationButton(){
+
+    $(window).bind('scroll',function(){
+        parallaxScroll()
+    })
+
+    function parallaxScroll(){
+        let scrolled = $(window).scrollTop();
+        $('#parallax-lvl-0').css('top',(0-(scrolled*.25))+'px');
+        $('#parallax-lvl-1').css('top',(0-(scrolled*.5))+'px');
+        $('#parallax-lvl-2').css('top',(0-(scrolled*.75))+'px');
+        $('#parallax-lvl-3').css('top',(0-(scrolled*.9))+'px');
+    }
+
 }
